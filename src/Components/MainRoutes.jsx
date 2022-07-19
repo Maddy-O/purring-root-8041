718b511265dfd4d9e30423d81a331f726fc8253a
swatibersurda <swatibersurda@gmail.com>
2022-07-19 20:54:48 +0530

    myfirstcommit

---
+++ src/Components/MainRoutes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Route,Routes} from "react-router-dom";
+import { CheckoutPage } from '../Pages/CheckoutPage';
+import FilterPage from '../Pages/FilterPage';
+
+export const MainRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/filterpage" element={<FilterPage/>}></Route>
+        <Route path='/checkoutpage' element={<CheckoutPage/>}></Route>
+    </Routes>
+  )
+}
