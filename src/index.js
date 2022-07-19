ba939ffffc3ad8bce343ceeb03929d20f6c54a8d
Maddy <95930594+Maddy-O@users.noreply.github.com>
2022-07-19 21:38:03 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -14,17 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
 import { ChakraProvider } from "@chakra-ui/react";
->>>>>>> origin
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< HEAD
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>
-=======
   <BrowserRouter>
     <Provider store={store}>
       <ChakraProvider>
@@ -32,5 +16,4 @@ root.render(
       </ChakraProvider>
     </Provider>
   </BrowserRouter>
->>>>>>> origin
 );
