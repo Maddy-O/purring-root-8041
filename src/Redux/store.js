80d4fef2fb37152ec97a2646acd073cd1da53c75
Maddy <95930594+Maddy-O@users.noreply.github.com>
2022-07-20 17:43:41 +0530

    Update

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,7 +2,12 @@ import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { reducer as AppReducer } from "./AppReducer/reducer";
 import { reducer as AuthReducer } from "./AuthReducer/reducer";
+import { reducer as LocationReducer } from "./LocationReducer/reducer";
 
-const rootReducer = combineReducers({ AppReducer, AuthReducer });
+const rootReducer = combineReducers({
+  AppReducer,
+  AuthReducer,
+  LocationReducer,
+});
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
+import Location from "../Pages/Location";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/loc" element={<Location />} />
     </Routes>
   );
 };
