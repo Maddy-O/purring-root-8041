384d0c7e1251c48194cc035823402acbd88e2374
Maddy <95930594+Maddy-O@users.noreply.github.com>
2022-07-21 22:43:49 +0530

    Update PickUpPage.jsx

--- src/Pages/PickUpPage.jsx
+++ src/Pages/PickUpPage.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Box, Button, Input, Flex, Text } from "@chakra-ui/react";
+import { FiArrowLeft } from "react-icons/fi";
 
 const PickUpPage = () => {
   return (
-    <Box>
-      <Box>Arrow</Box>
-      <Flex>
-        <Input />
+    <Box padding={"20px"}>
+      <Box>
+        <FiArrowLeft size="25px" />
+      </Box>
+      <Flex gap="15px">
+        <Box>
+          <Input
+            variant={"outline"}
+            border={"1px solid gray"}
+            boxShadow="0px 0px 0px 1px gray"
+            placeholder="Select your starting point"
+            color="black"
+            backgroundColor="white"
+          />
+        </Box>
         <Box>
           <Text>Current Location</Text>
         </Box>
@@ -19,10 +31,13 @@ const PickUpPage = () => {
           <Text>SUGGESTED PICK UP LOCATIONS</Text>
         </Box>
         <Box>
-          <Text>Banglore International Airport</Text>
+          <Text>Airport</Text>
         </Box>
         <Box>
-          <Text>Yelahanka Railway Station, Yelahanka, 3127564</Text>
+          <Text>Railway Station</Text>
+        </Box>
+        <Box>
+          <Text>City Bus Station</Text>
         </Box>
       </Box>
     </Box>
