460910cef2ed014aaab02350e65f3d0c1e17e4a7
swatibersurda <swatibersurda@gmail.com>
2022-07-22 13:06:03 +0530

    addingckeckoutui

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import { Box,Text,Image } from '@chakra-ui/react' 
+import { Box,Text,Image, List } from '@chakra-ui/react' 
 import { useSelector } from 'react-redux'
+import {ArrowBackIcon, StarIcon} from "@chakra-ui/icons"
+import {Link as RouterLink} from "react-router-dom"
+import {FcEmptyBattery} from "react-icons/fa"
+import { FaRegBuilding } from "react-icons/fa"
 
 export const CheckoutPage = () => {
   const filterData=useSelector((state)=>state.AppReducer.filterData)
@@ -22,9 +26,15 @@ export const CheckoutPage = () => {
   //  '/ 
 
   return (
-    <Box width={"100%"} height={"500px"} border={"1px solid black"}>
+    <Box width={"100%"} height={"550px"} border={"1px solid black"}>
+     
       {/* first box */}
-      <Box width={"100%"} height={"50px"} bg={"#f5f5f5"}></Box>
+      <Box width={"100%"} height={"50px"} bg={"#f5f5f5"} textAlign={"left"}>
+        <RouterLink to="/filterpage" >
+        <ArrowBackIcon w={30} h={45}></ArrowBackIcon>
+        </RouterLink>
+      
+      </Box>
       {/* second box */}
       <Box width={"100%"} marginTop={"0%"} height={"90px"} display={"flex"} border={"1px solid transparent"} marginTop={"-1"}>
         <Box width={"25%"} height={"100%"} >
@@ -38,6 +48,39 @@ export const CheckoutPage = () => {
           <Image src={filterData.image} width={"100%"} height={"100%"}></Image>
            
         </Box>
+          
+          {/* total div */}
+          </Box>
+          <Box textAlign={"left"} padding={"1%"} marginLeft={"25%"} width={"50%"} height={"300px"} border={"1px solid none"}
+            boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px;"}
+           >
+          
+            <Box width={"100%"} marginBottom={"3%"}>
+              <Text fontSize={"18px"}>Fri,22 Jul,12:00 PM</Text>
+            <Text fontSize={"12px"}>New Delhi Railway Station</Text>
+            </Box>
+            
+          
+            <Text fontSize={"18px"}>Fri,22 Jul,8:00  PM</Text>
+            <Text fontSize={"12px"}>New Delhi Railway Station</Text>
+            <Box display={"flex"}>
+            <Image src={"https://zoomcar-assets.zoomcar.com/images/original/ccedfb3dddda918d76ad27240ffa582f4a59a157.png?1584454852"} 
+            alt="sf" width={"5%"} height={"20px"} marginTop={"5%"}/>
+         <Text fontSize={"16px"} marginLeft={"2%"} marginTop={"4.5%"}>Unlimited Kms without Fuel</Text> 
+            </Box>
+            <Box display={"flex"} width={"100%"} marginTop={"2%"}>
+             <StarIcon w={"6"} h={"8"} color={"gold"}></StarIcon>
+              <Text fontSize={"14px"} marginLeft={"2%"} marginTop={"2%"}>3.7 (6) . {filterData.Driven}</Text>
+            </Box>
+            <Box  
+            fontSize={["10px","12px"]} padding={"12px 40px 12px 16px"} marginTop={"16px"} color={"#5160C2"} bg={"#f0f4ff"}
+             borderRadius={"4px"} marginTop={"4%"} width={"100%"}
+              height={[20,50,50]} border={"none"}>
+              you will get a clean and sanitized car.Exact car location will be shared post-booking
+            </Box>
+            <Box marginTop={"4%"} width={"100%"} border={"1px solid crimson"} >
+              <Text fontWeight={"600"}>Damage Protection Package</Text>
+            </Box>
           
       </Box>
     </Box>
