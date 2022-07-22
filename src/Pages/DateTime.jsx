2704076012ef9742a47e962e5b08f3c33396ef2b
Maddy <95930594+Maddy-O@users.noreply.github.com>
2022-07-22 20:02:16 +0530

    Update

--- src/Pages/DateTime.jsx
+++ src/Pages/DateTime.jsx
@@ -2,6 +2,7 @@ import { Box, Container } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { DateRange, DateRangePicker } from "react-date-range";
 import { addDays } from "date-fns";
+import { ThemeProvider } from "styled-components";
 
 const DateTime = () => {
   let handleSelect = (date) => {
@@ -15,25 +16,32 @@ const DateTime = () => {
     },
   ]);
   return (
-    <>
-      <Container>
-        <Box>
-          <DateRange onInit={handleSelect} onChange={handleSelect} />
-        </Box>
-      </Container>
-      <Box>
-        <DateRangePicker
-          onChange={(item) => setState([item.selection])}
-          showSelectionPreview={true}
-          moveRangeOnFirstSelection={false}
-          months={2}
-          ranges={state}
-          direction="horizontal"
-          preventSnapRefocus={true}
-          calendarFocus="backwards"
-        />
-      </Box>
-    </>
+    <ThemeProvider
+      theme={{
+        breakpoints: ["32em", "48em", "64em"],
+        reactDatepicker: {
+          daySize: [36, 40],
+          fontFamily: "system-ui, -apple-system",
+          colors: {
+            accessibility: "#D80249",
+            selectedDay: "#f7518b",
+            selectedDayHover: "#F75D95",
+            primaryColor: "#d8366f",
+          },
+        },
+      }}
+    >
+      <DateRangePicker
+        onChange={(item) => setState([item.selection])}
+        showSelectionPreview={true}
+        moveRangeOnFirstSelection={false}
+        months={2}
+        ranges={state}
+        direction="horizontal"
+        preventSnapRefocus={true}
+        calendarFocus="backwards"
+      />
+    </ThemeProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <ChakraProvider theme={"none"}>
+      <ChakraProvider>
         <App />
       </ChakraProvider>
     </Provider>
