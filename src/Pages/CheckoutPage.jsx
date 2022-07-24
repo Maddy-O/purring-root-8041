8f6fcfcc4a05830858f98f314a85700f2ab1622b
Maddy <95930594+Maddy-O@users.noreply.github.com>
2022-07-24 11:54:46 +0530

    Update

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -100,7 +100,6 @@ export const CheckoutPage = () => {
           bg={"#f0f4ff"}
           borderRadius={"4px"}
           width={"100%"}
-          height={[20, 50, 50]}
           border={"none"}
         >
           you will get a clean and sanitized car.Exact car location will be
@@ -126,48 +125,6 @@ export const CheckoutPage = () => {
               ₹{filterData.Price + 509}
             </Text>
           </Box>
-<<<<<<< HEAD
-          <Box textAlign={"left"} padding={"1%"} marginLeft={"25%"} width={"50%"} height={"300px"} border={"1px solid none"}
-          
-           >
-          
-            <Box width={"100%"} marginBottom={"3%"}>
-              <Text fontSize={"18px"}>Fri,22 Jul,12:00 PM</Text>
-            <Text fontSize={"12px"}>New Delhi Railway Station</Text>
-            </Box>
-            
-          
-            <Text fontSize={"18px"}>Fri,22 Jul,8:00  PM</Text>
-            <Text fontSize={"12px"}>New Delhi Railway Station</Text>
-            <Box display={"flex"}>
-            <Image src={"https://zoomcar-assets.zoomcar.com/images/original/ccedfb3dddda918d76ad27240ffa582f4a59a157.png?1584454852"} 
-            alt="sf" width={"5%"} height={"20px"} marginTop={"5%"}/>
-         <Text fontSize={"16px"} marginLeft={"2%"} marginTop={"4.5%"}>Unlimited Kms without Fuel</Text> 
-            </Box>
-            <Box display={"flex"} width={"100%"} marginTop={"1%"}>
-             <StarIcon w={"6"} h={"8"} color={"gold"}></StarIcon>
-              <Text fontSize={"14px"} marginLeft={"2%"} marginTop={"2%"}>{filterData.Star} (6) . {filterData.Driven}</Text>
-            </Box>
-            <Box  
-            fontSize={["10px","12px"]} padding={"12px 40px 12px 16px"} marginTop={"16px"} color={"#5160C2"} bg={"#f0f4ff"}
-             borderRadius={"4px"} marginTop={"4%"} width={"100%"}
-              border={"none"}>
-              you will get a clean and sanitized car.Exact car location will be shared post-booking
-            </Box>
-            <Box marginTop={"1%"} width={"100%"} fontSize={"19px"}
-             border={"1px solid none"} >
-              <Text fontWeight={"600"}>Damage Protection Package</Text>
-               <Text fontSize={"14px"} marginBottom={"0.3%"}>Standard (₹509)</Text>
-               <Text fontSize={"10px"}>You pay up to INR 3499 in case of any damage</Text>
-            </Box>
-            <Box marginTop={"2%"} 
-             width={"100%"} height={"70px"}>
-              <Box display={"flex"}>
-              <Text fontWeight={"600"} fontSize={"18px"}>₹{filterData.Price+509}</Text> 
-              
-=======
->>>>>>> f412768c46b70e23dc7c17ceb41c8c2d0d41c848
-
           <Button
             fontSize={"20px"}
             width={"100%"}
